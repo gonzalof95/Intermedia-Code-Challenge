463f0736d4716422758ac4c69284ff89379c2979
gonzaloif-py <gonzalo.fuentes@pedidosya.com>
2020-12-19 15:07:47 -0300

    rearranging main screen

---
+++ Intermedia Code Challenge/Intermedia Code Challenge/App/Hero Screen/Presentation/View/ViewController/HeroViewController+Extension.swift
@@ -0,0 +1,41 @@
+//
+//  HeroViewController+Extension.swift
+//  Intermedia Code Challenge
+//
+//  Created by Gonzalo Ivan Fuentes on 19/12/2020.
+//
+
+import UIKit
+
+extension HeroViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return heroesArray.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as! HeroCell
+        let hero = heroesArray[indexPath.row]
+        cell.set(hero)
+
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
+extension HeroViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let offsetY = scrollView.contentOffset.y
+        let contentHeight = scrollView.contentSize.height
+
+        if offsetY > contentHeight - scrollView.frame.height {
+            if !apiCalling {
+                apiCalling = true
+                timesRecalled += 1
+                presenter?.requestMoreData(timesRecalled * 15, true)
+            }
+        }
+    }
+}
